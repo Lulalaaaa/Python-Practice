1e8eb55d8ffd7277c91ad19bea4e6130a2eeebf1
Lulalaaaa <145569444+Lulalaaaa@users.noreply.github.com>
2023-09-20 20:54:08 +0800

    Add files via upload

---
+++ 2023.9.20 Practice.py
@@ -0,0 +1,19 @@
+#2023.9.20
+#for循环、if语句
+a=[1,2,3,4,5,6,7,8]
+for i in a:
+    print(i)
+    print("hello")
+
+#函数
+#定义一个函数
+def addNum(a,b):
+    return a+b
+print(addNum(2,3))
+
+def square(x):
+    return x*x
+print(square(3))
+
+#模块导入
+import
